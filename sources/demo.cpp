40309ea3e3e10e4e329edce45a5dde69b324b890
DespiteDeath <Kogut_nikita@mail.ru>
2017-11-29 18:47:37 +0300

    Update demo.cpp

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -1,10 +1,56 @@
 #include <print.hpp>
+#include <boost/program_options.hpp>
 
-int main(int argc, char** argv) {
-std::string text;
-while(std::cin >> text) {
-std::ofstream out("log.txt", std::ios_base::app);
-print(text, out);
-out << std::endl;
-}
+namespace po = boost::program_options;
+
+int main(int argc, const char* argv[]) {
+
+    std::string text;
+    std::string filename;  //filename including directory
+    std::string configname(std::getenv("HOME"));  //config filename including directory
+    configname += "/.config/demo.cfg";
+
+    po::options_description options("Command line options");
+    options.add_options()
+            ("help,h", "print help message")
+            ("output", po::value<std::string>(), "file to print to");
+
+    std::ifstream config(configname);
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, options), vm);
+    po::store(po::parse_environment(options, "DEMO_"), vm);
+    po::store(po::parse_config_file(config, options), vm);
+    po::notify(vm);
+
+    config.close();
+
+    if (vm.count("help") != 0)
+    {
+        std::cout << options << std::endl;
+        return 1;
+    }
+
+    std::string tmp;
+    while(std::cin >> tmp)
+    {
+        text += tmp;
+        if(!std::cin.eof())
+        {
+            text += " ";
+        }
+    }
+
+    if(vm.count("output") != 0)
+    {
+        std::ofstream out(vm["output"].as<std::string>());
+        print(text, out);
+        out.close();
+    } else {
+        std::ofstream out("default.log");
+        print(text, out);
+        out.close();
+    }
+
+    return 0;
 }
